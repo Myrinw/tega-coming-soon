359b3addc7ebd9c22f243901ba6be4b1899501d3
firefly <myrin.w@outlook.com>
2023-07-19 01:07:04 +0200

    update

--- src/components/hero-main/hero-main.js
+++ src/components/hero-main/hero-main.js
@@ -1,32 +1,18 @@
 function HeroMain(props) {
 	return (
 		<section className="hero hero--main">
-			<div className="container">
-				<img
-					className="coming-logo"
-					src={require("../../img/logo.png")}
-					alt=""
-				/>
-				<img
-					className="construction-logo"
-					src={require("../../img/construction.png")}
-					alt=""
-				/>
-				<p className="hero__text">
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-					<br />
-				</p>
-				{/* <img
+			<img className="coming-logo" src={require("../../img/logo.png")} alt="" />
+			<img
+				className="construction-logo"
+				src={require("../../img/construction.png")}
+				alt=""
+			/>
+
+			{/* <img
 					className="hero__image"
 					src={require("../../img/homegraphs.png")}
 					alt=""
 				/> */}
-			</div>
 		</section>
 	);
 }
